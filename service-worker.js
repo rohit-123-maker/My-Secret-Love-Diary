74c999dec44b0d73d7562e8a02d0ebe9912a2d02
Rohit Vishwakarma <rv4173167@gmail.com>
2026-05-03 20:50:07 +0530

    Add service worker for caching resources

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "love-diary-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
